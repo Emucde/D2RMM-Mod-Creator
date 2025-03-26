487b58773bc09e49c9163b47673ea6c5a5d24cf5
Emanuel Huber <emanuel.huber@student.tuwien.ac.at>
2025-03-26 14:48:46 +0100

    added gitignore and autodetect of files

--- cubemain_convert.c
+++ cubemain_convert.c
@@ -1,11 +1,54 @@
+#include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#include <windows.h>
+
+#define MAX_FILES 100
+#define MAX_FILENAME 256
 
 #define MAX_LINE_LENGTH 40960
 #define MAX_FIELDS 2000
+
+int get_txt_files(char files[][MAX_FILENAME]) {
+    WIN32_FIND_DATA findFileData;
+    HANDLE hFind;
+    int fileCount = 0;
+
+    // Get the path of the current executable
+    char currentDirectory[MAX_PATH];
+    GetModuleFileName(NULL, currentDirectory, MAX_PATH);
+
+    // Remove the executable name to get the folder path
+    char* lastSlash = strrchr(currentDirectory, '\\');
+    if (lastSlash) {
+        *lastSlash = '\0';
+    }
+
+    // Append wildcard to search for .txt files
+    char searchPath[MAX_PATH];
+    snprintf(searchPath, sizeof(searchPath), "%s\\*.txt", currentDirectory);
+
+    // Start searching for .txt files
+    hFind = FindFirstFile(searchPath, &findFileData);
+    if (hFind == INVALID_HANDLE_VALUE) {
+        return 0;
+    }
+
+    do {
+        if (!(findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+            strncpy(files[fileCount], findFileData.cFileName, MAX_FILENAME - 1);
+            files[fileCount][MAX_FILENAME - 1] = '\0';  // Ensure null-termination
+            fileCount++;
+
+            if (fileCount >= MAX_FILES) {
+                break;  // Prevent array overflow
+            }
+        }
+    } while (FindNextFile(hFind, &findFileData) != 0);
+
+    FindClose(hFind);
+    return fileCount;
+}
 
 char** split_tabs(const char* line, int* count) {
     char** fields = malloc(MAX_FIELDS * sizeof(char*));
@@ -34,6 +77,14 @@ void escape_quotes(char* str) {
 }
 
 int main(int argc, char* argv[]) {
+
+    char files[MAX_FILES][MAX_FILENAME];
+    int fileCount = get_txt_files(files);
+
+    printf("Found %d .txt files:\n", fileCount);
+    for (int i = 0; i < fileCount; i++) {
+        printf("%s\n", files[i]);
+    }
 
     FILE* input = fopen("cubemain.txt", "r");
     FILE* output = fopen("mod_cubemain.js", "w");
